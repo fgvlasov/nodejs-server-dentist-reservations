ec88ea1d2d7464de6ae8897317491ad294dc60e6
fgvlasov <fgvlasov@gmail.com>
2023-11-08 15:29:26 +0200

    JWT token

--- index.js
+++ index.js
@@ -2,12 +2,38 @@
 
 var path = require("path");
 var http = require("http");
+var express = require("express");
+var jwt = require("jsonwebtoken");
 
 var oas3Tools = require("oas3-tools");
 var serverPort = 8080;
 
 // Import your fake data
 var data = require("./data");
+
+// Create an Express app
+var app = express();
+
+// Middleware to check JWT token
+const authenticateJWT = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+// Use the authentication middleware
+app.use(authenticateJWT);
 
 // swaggerRouter configuration
 var options = {
@@ -22,9 +48,11 @@ var expressAppConfig = oas3Tools.expressAppConfig(
   options,
   data
 );
-var app = expressAppConfig.getApp();
 
-// Initialize the Swagger middleware
+// Include the Swagger middleware
+app.use(expressAppConfig.getApp());
+
+// Start the server
 http.createServer(app).listen(serverPort, function () {
   console.log(
     "Your server is listening on port %d (http://localhost:%d)",
