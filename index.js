ed97f521e5e373b76a20ec481dda91ec9ba09140
fgvlasov <fgvlasov@gmail.com>
2023-11-08 15:24:42 +0200

    added data

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-'use strict';
+"use strict";
 
-var path = require('path');
-var http = require('http');
+var path = require("path");
+var http = require("http");
 
-var oas3Tools = require('oas3-tools');
+var oas3Tools = require("oas3-tools");
 var serverPort = 8080;
+
+// Import your fake data
+var data = require("./data");
 
 // swaggerRouter configuration
 var options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
+  routing: {
+    controllers: path.join(__dirname, "./controllers"),
+  },
 };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+// Pass the fake data to the expressAppConfig
+var expressAppConfig = oas3Tools.expressAppConfig(
+  path.join(__dirname, "api/openapi.yaml"),
+  options,
+  data
+);
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  console.log(
+    "Your server is listening on port %d (http://localhost:%d)",
+    serverPort,
+    serverPort
+  );
+  console.log(
+    "Swagger-ui is available on http://localhost:%d/docs",
+    serverPort
+  );
 });
-
